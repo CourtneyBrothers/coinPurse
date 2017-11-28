a2677a9fe4104aa178a4b929ebe4d9b3f781b0a9
Courtney Brothers <cournteywbrothers@gmail.com>
2017-11-27 20:02:04 -0600

    more attempts

--- main.js
+++ main.js
@@ -19,20 +19,39 @@ function coinCounter (coinAmount) {
   // Initialize a JavaScript object to hold the coins
   var coinPurse = {};
 
-  let numberOfQuarters = coinAmount % quarter;
-  console.log(numberOfQuarters);
-  coinAmount = coinAmount - (numberOfQuarters * quarter);
-  let numberOfDimes = coinAmount % dime;
-  coinAmount = coinAmount - (numberOfDimes * dime);
-  let numberOfNickels = coinAmount % nickel;
-  coinAmount = coinAmount - (numberOfNickels * nickel);
-  let numberOfPennies = coinAmount % penny;
+  //let numberOfQuarters = coinAmount % quarter;
+  //console.log(numberOfQuarters);
+  //console.log( 890 % 25); //Why is this two different numbers?
+  for (var i =0; j=0, k=0, l=0; i < 4; i++) {
+      if (coinAmount > 0.25) {
+        coinAmount = coinAmount - quarter;
+        console.log(coinAmount);
+        i++;
+      } 
+  for (0.10 < coinAmount < 0.25){
+        coinAmount = coinAmount - dime;
+        j++;
+      } else if (0.05 < coinAmount < 0.10){
+        coinAmount = coinAmount - nickel;
+        k++;
+      } else if (0.01 < coinAmount < 0.05){
+        coinAmount = coinAmount - penny;
+        l++;
+      }
+      break;
+    }
+  //coinAmount = coinAmount - (numberOfQuarters * quarter);
+  //let numberOfDimes = coinAmount % dime;
+  //coinAmount = coinAmount - (numberOfDimes * dime);
+  //let numberOfNickels = coinAmount % nickel;
+  //coinAmount = coinAmount - (numberOfNickels * nickel);
+  //let numberOfPennies = coinAmount % penny;
 
   
-  coinPurse.quarters = numberOfQuarters;
-  coinPurse.dimes = numberOfDimes;
-  coinPurse.nickel = numberOfNickels;
-  coinPurse.pennies = numberOfPennies;
+  coinPurse.quarters = i;
+  coinPurse.dimes = j;
+  coinPurse.nickel = k;
+  coinPurse.pennies = l;
 
   return coinPurse;
 }
